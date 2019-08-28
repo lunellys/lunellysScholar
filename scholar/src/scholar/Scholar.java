ea703a7429ffff7fdd4ce6048eb77f505e274448
lunellys paola <lunepaocortes@hotmail.com>
2019-08-28 08:45:34 -0500

    lunellys paola

--- scholar/src/scholar/Scholar.java
+++ scholar/src/scholar/Scholar.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +16,32 @@ public class Scholar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Empleado empleado1=new Empleado();
+        empleado1.nombre= "Lunellys paola";
+        empleado1.apellido= "Cortes montalvo";
+        empleado1.fecha_nacimiento="02 de mayo";
+        empleado1.tipo_sangre="O+";
+        
+        
+        //System.out.println("El empleado se llama " +empleado1.nombre);
+        
+        //System.out.println("El apellido del empleado es "+empleado1.apellido);
+        empleado1.printName();
+        System.out.println("La fecha de nacimiento es"+empleado1.fecha_nacimiento);
+        System.out.println("El tipo de sangre es"+empleado1.tipo_sangre);
+        
+        Estudiante estudiante1=new Estudiante();
+        estudiante1.nombre= "Marcos Elias ";
+        estudiante1.apellido= "Caro Meza";
+        
+                
+        //System.out.println("El estudiante se llama " +estudiante1.nombre);
+        //System.out.println("El apellido del estudiante es " +estudiante1.apellido);
+        estudiante1.printName();
+        
+        
+        
+        
     }
     
 }
